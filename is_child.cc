36289bc39ffa832ffea3fa635690d1f2806966b7
Igor Kolupaev <ikolupaev@gmail.com>
2016-01-26 13:59:18 +0000

    fixed code style

--- is_child.cc
+++ is_child.cc
@@ -1,38 +1,35 @@
 #include <iostream>
 
-struct node
-{
+struct node {
     node *left;
     node *right;
 };
 
-bool is_child( node *head, node *n);
+bool is_child(node *head, node *n);
 
-int main()
-{
-   auto head = new node();
+int main() {
+    auto head = new node();
 
-   head->left = new node();
-   head->right = new node();
+    head->left = new node();
+    head->right = new node();
 
-   head->left->left = new node();
+    head->left->left = new node();
 
-   head->right->left = new node();
-   head->right->right = new node();
+    head->right->left = new node();
+    head->right->right = new node();
 
-   auto deattached_node = new node();
+    auto deattached_node = new node();
 
-   std::cout << "h.r.r: " << is_child( head, head->right->right ) << std::endl;
-   std::cout << "h.l: " << is_child( head, head->left ) << std::endl;
-   std::cout << "deattached: " << is_child( head, deattached_node ) << std::endl;
+    std::cout << "h.r.r: " << is_child(head, head->right->right) << std::endl;
+    std::cout << "h.l: " << is_child(head, head->left) << std::endl;
+    std::cout << "deattached: " << is_child(head, deattached_node) << std::endl;
 }
 
-bool is_child(node *head, node *n )
-{
-    if( head == n ) return true;
-    if( head == NULL ) return false;
-    if( is_child( head->left, n ) ) return true;
-    if( is_child( head->right, n ) ) return true;
+bool is_child(node *head, node *n) {
+    if ( head == n ) return true;
+    if ( head == NULL ) return false;
+    if ( is_child(head->left, n) ) return true;
+    if ( is_child(head->right, n) ) return true;
 
     return false;
 }
