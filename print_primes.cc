70bf8aa7e0b5768f9c31e5f4e7704bd7ab37791a
Igor Kolupaev <ikolupaev@hotmail.com>
2016-02-03 22:32:25 +0300

    refactored print primes with Sieve of Eratosthenes approach

--- print_primes.cc
+++ print_primes.cc
@@ -1,22 +1,25 @@
 #include <iostream>
-
-int calc_next_prime(int number) {
-    number++;
-    int divider = 2;
-    while ( number != divider ) {
-        if ( number % divider == 0 ) {
-            divider = 1;
-            number++;
-        }
-        
-        divider++;
-    } 
-}
+#include <cstring>
 
 int main() {    
-    int num = 1;
-    for( int i = 0; i < 100; i++ ) {
-        num = calc_next_prime( num );
-        std::cout << num << std::endl;
+    uint64_t size = 1000000;
+    bool primes[size];
+    for( int i = 0; i < size; i++ ) primes[i] = true;
+
+    uint64_t next_prime = 2;
+    while( next_prime < size ) {
+        
+        uint64_t n = next_prime*2;
+        while( n < size ) {
+            primes[n] = false;
+            n+=next_prime;
+        }
+        
+        next_prime++;
+        while( !primes[next_prime] ) next_prime++;
+    }
+
+    for( uint64_t i = 2; i < size; i++ ) {
+        if( primes[i] ) std::cout << i << std::endl;
     }
 }
