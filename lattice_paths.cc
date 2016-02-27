c6af47d0d205e8d351b4301bef945d264cfec296
Igor Kolupaev <ikolupaev@gmail.com>
2016-02-27 14:03:09 +0300

    refactored lattice paths

--- lattice_paths.cc
+++ lattice_paths.cc
@@ -1,35 +1,19 @@
 #include <iostream>
 #include <thread>
-#include <vector>
-#include <numeric>
 
 using namespace std;
 
-void lattice_paths_counter( int r, int c, int rows, int cols, uint64_t &result ) {
+int lattice_paths_counter( int rows, int cols ) {
 
-    if( r == rows && c == cols ) result += 1;
+    if( rows == 0 || cols == 0 ) return 1;
 
-    if( r < rows )
-        lattice_paths_counter( r+1, c, rows, cols, result );
-
-    if( c < cols )
-        lattice_paths_counter( r, c+1, rows, cols, result );
+    int rr = 0;
+    rr += lattice_paths_counter( rows - 1, cols );
+    rr += lattice_paths_counter( rows, cols - 1 );
+        
+    return rr;
 } 
 
 int main() {
-    int rows = 10;
-    int columns = rows;
-    
-    vector<thread*> tt;
-    vector<uint64_t> result = {0,0};
-    
-    tt.push_back( new thread( lattice_paths_counter, 1, 0, rows, columns, ref(result[0]) ) );
-    tt.push_back( new thread( lattice_paths_counter, 0, 1, rows, columns, ref(result[1]) ) );
-
-    for( thread *t : tt ) {
-        t->join();
-        delete t;
-    }
-
-    cout << accumulate( result.begin(), result.end(), 0 ) << endl;
+    cout << lattice_paths_counter( 2, 2 ) << endl;
 }
