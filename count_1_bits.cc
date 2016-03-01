365ea7bd938b4ffef6aed4364252b0a8344737f1
Igor Kolupaev <ikolupaev@gmail.com>
2016-03-01 09:07:44 +0300

    refactored bits counter

--- count_1_bits.cc
+++ count_1_bits.cc
@@ -4,8 +4,8 @@ int count_1_bits( int n ) {
 
     int count = 0;
     while ( n != 0 ) {
-        count += n & 1;
-        n >>= 1;
+        n &= n - 1;
+        count++;
     } 
     
     return count;
---
+++ lattice_paths_single.cc
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <thread>
+
+using namespace std;
+
+int lattice_paths_counter( int rows, int cols ) {
+
+    if( rows == 0 || cols == 0 ) return 1;
+
+    int rr = 0;
+    rr += lattice_paths_counter( rows + 1, cols );
+    rr += lattice_paths_counter( rows, cols + 1 );
+        
+    return rr;
+} 
+
+int main() {
+    
+    cout << lattice_paths_counter(0,0,2,2) << endl;
+}
